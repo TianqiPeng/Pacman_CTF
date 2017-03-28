f6489fcaa73474439c9b82b57f38d26d109fa3dc
Tanner Fiez <fiezt@uw.edu>
2017-03-28 15:33:12 -0700

    Update myTeam.py

--- Code/myTeam.py
+++ Code/myTeam.py
@@ -35,14 +35,6 @@ def createTeam(firstIndex, secondIndex, isRed, first='OffensiveAgent',
 	team, initialized using firstIndex and secondIndex as their agent
 	index numbers.  isRed is True if the red team is being created, and
 	will be False if the blue team is being created.
-
-	As a potentially helpful development aid, this function can take
-	additional string-valued keyword arguments ("first" and "second" are
-	such arguments in the case of this function), which will come from
-	the --redOpts and --blueOpts command-line arguments to capture.py.
-	For the nightly contest, however, your team will be created without
-	any extra arguments, so you should make sure that the default
-	behavior is what you want for the nightly contest.
 	"""
 
 	return [eval(first)(firstIndex), eval(second)(secondIndex)]
